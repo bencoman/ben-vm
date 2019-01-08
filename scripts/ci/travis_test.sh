38125ac55f04ad8f792148da376e97fd64d6b88e
Ben Coman <btc@openInWorld.com>
2019-01-08 22:21:19 +0800

    Add travis_build.sh PLATFORM=mingw_on_linux

--- scripts/ci/travis_test.sh
+++ scripts/ci/travis_test.sh
@@ -2,8 +2,12 @@
 set -e
 
 # Don't run tests on ARM (ARM builds already take a long time)
-[[ "${ARCH}" == *"ARM"* ]] && exit 0
+[[ "${ARCH}" == *"ARM"*   ]] && exit 0
 
+# Can't test a Windows executable on a Linux box
+[[ "${ARCH}" == *"mingw"* ]] && exit 0
+
+# Test these...
 case "${FLAVOR}" in
   "squeak"*|"pharo"*)
     "./tests/smalltalkCI.sh"
